7f18491080df59063d99d867301554fcbd9f466c
ethio-man <natymiskir@gmail.com>
2025-08-01 09:53:03 -0300

    c

---
+++ x.js
@@ -0,0 +1,11 @@
+// POST /api/return
+//return 401 if the user not logged in
+//return 400 if valid customerId not provided
+//return 400 if vaild movieId not provided
+//return 404 if  the rental not found
+//return 400 if the rental already processed
+//return 200 if valid request
+// set the return date
+//calculate the rental fee
+//increase the stock
+//return the Rental
